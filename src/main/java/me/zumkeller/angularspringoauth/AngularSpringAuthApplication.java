b66ab6e2052349c36a6bb74765c5f609a5e470d8
Stephan Zumkeller <stephan.zumkeller@capgemini.com>
2022-10-01 10:45:17 +0200

    Initialized with Spring

---
+++ src/main/java/me/zumkeller/angularspringoauth/AngularSpringAuthApplication.java
@@ -0,0 +1,13 @@
+package me.zumkeller.angularspringoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularSpringAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/zumkeller/angularspringoauth/AngularSpringAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zumkeller.angularspringoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
