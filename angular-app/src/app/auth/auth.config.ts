53d535d21913bf4373f759d8a94e8f961756155b
Stephan Zumkeller <stephan.zumkeller@capgemini.com>
2022-10-02 22:09:42 +0200

    Fix issuer config

--- angular-app/src/app/auth/auth.config.ts
+++ angular-app/src/app/auth/auth.config.ts
@@ -2,7 +2,8 @@ import {AuthConfig} from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
   // Url of the Identity Provider
-  issuer: 'http://localhost:9090/realms/oauth-demo-realm',
+  issuer: 'http://host.docker.internal:9090/realms/oauth-demo-realm',
+  requireHttps: false,
 
   // URL of the SPA to redirect the user to after login
   redirectUri: 'http://localhost:4200/',
